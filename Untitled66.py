9152f1899d93b88a02cb785536a10a667273cbb4
Rajandep singh matharu <105154536+Rajn013@users.noreply.github.com>
2023-05-16 13:20:54 +0530

    Add files via upload

---
+++ Untitled66.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1. What is the meaning of multiple inheritance?
+# 
+
+# 
+# Multiple inheritance refers to a feature in object-oriented programming languages where a class can inherit from more than one parent class. It allows a subclass to inherit attributes and methods from multiple parent classes, enabling code reuse and flexibility in class design.
+
+# Q2. What is the concept of delegation?
+# 
+
+# 
+# Delegation is like asking someone else to do a specific task for you. Instead of doing the task yourself, you pass it on to another person who is specialized in that area. In programming, it means that one object asks another object to perform a certain task on its behalf.
+
+# Q3. What is the concept of composition?
+# 
+
+# Composition in programming is like building something using smaller building blocks. Instead of creating one big object, you create it by combining smaller objects together. It's similar to how you can build a car using an engine, wheels, and chassis.
+# composition allows you to build complex objects by combining smaller objects together. It promotes code reusability, modularity, and flexibility, allowing objects to work together and creating hierarchical relationships.
+
+# Q4. What are bound methods and how do we use them?
+# 
+
+# A bound method is a function that is associated with an instance of a class. It is created when you access a method of an object. Bound methods have access to the instance's data and can operate on it.
+# 
+# To use a bound method, you simply call it like a regular function, but you do so on an instance of a class. When you call a bound method, the instance is automatically passed as the first argument, commonly referred to as self.
+
+# Q5. What is the purpose of pseudoprivate attributes?
+# 
+
+#  pseudoprivate attributes in Python provide a way to create a form of private access within a class, ensuring that attributes are not accidentally accessed or overridden by subclasses or external code. They help with encapsulation and maintaining the internal integrity of a class.
+
+# In[ ]:
+
+
+
+
